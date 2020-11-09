258c9592c7385c908ca4b6c763fb724113802912
Henriquegab <67250181+Henriquegab@users.noreply.github.com>
2020-11-09 08:47:10 -0300

    Create formulario_senha.php

---
+++ formulario_senha.php
@@ -0,0 +1,41 @@
+<?php 
+include("cabecalho.php");
+
+?>
+
+<?php #formulario de login ?>
+
+	<h2>Recuperação de senha</h2>
+		<form action="senha.php" method="post">
+			<table class="table">
+				<tr>
+					<td>Digite seu CPF (Somente números)</td>
+					<td><input class="form-control" type="number" name="CPF"></td>
+				</tr>
+				<tr>
+					<td>Digite seu Email</td>
+					<td><input class="form-control" type="text" name="email"></td>
+				</tr>
+				<tr>
+				<td><button type="submit" class="btn btn-primary">Login</button></td>
+				</tr>
+			</table>
+		</form>
+
+		<form action="index.php" method="post">
+			<table class="table">
+				<tr>
+				<td><button type="submit" class="btn btn-primary">Voltar</button></td>
+				</tr>
+			</table>
+		</form>
+
+
+
+
+
+
+
+<?php
+include ("rodape.php");
+?>
