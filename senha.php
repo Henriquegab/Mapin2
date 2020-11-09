bc637c935d954905c8a1a4f3bf39a4cc3f02d14f
Henriquegab <67250181+Henriquegab@users.noreply.github.com>
2020-11-09 08:47:51 -0300

    Update senha.php

--- senha.php
+++ senha.php
@@ -1,41 +1,12 @@
 <?php 
-include("cabecalho.php");
-
-?>
-
-<?php #formulario de login ?>
-
-	<h2>Recuperação de senha</h2>
-		<form action="recuperar_senha.php" method="post">
-			<table class="table">
-				<tr>
-					<td>Digite seu CPF (Somente números)</td>
-					<td><input class="form-control" type="number" name="CPF"></td>
-				</tr>
-				<tr>
-					<td>Digite seu Email</td>
-					<td><input class="form-control" type="text" name="email"></td>
-				</tr>
-				<tr>
-				<td><button type="submit" class="btn btn-primary">Login</button></td>
-				</tr>
-			</table>
-		</form>
-
-		<form action="index.php" method="post">
-			<table class="table">
-				<tr>
-				<td><button type="submit" class="btn btn-primary">Voltar</button></td>
-				</tr>
-			</table>
-		</form>
+include("conexao.php");
+include("recuperar_senha.php");
 
 
+$recupera = RecuperarUsuario($conexao,$_POST["CPF"],$_POST["email"]);
 
+if ($recupera) {
+	
+}
 
-
-
-
-<?php
-include ("rodape.php");
 ?>
